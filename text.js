509d38ca8e53494a757751e0387184763b6c9446
Master TLM <110680604+MasterTLM@users.noreply.github.com>
2023-04-21 00:46:14 +0700

    update

--- text.js
+++ text.js
@@ -8,12 +8,6 @@ function myFunction3() {
         document.getElementById("info").innerHTML = "<div class='alert alert-success'><a id='link' href='https://vm.tiktok.com/ZMYvefdQG/' target='_blank'> <b>Tiktok: Minh Master</b> </a> </div>";
 }
 function myFunction4() {
-        document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>Số điện thoại: 0767922149</b> </div>";
-}
-function myFunction5() {
-        document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>E-Mail: tranleminh106@gmail.com</b> </div>";
-}
-function myFunction6() {
         document.getElementById("info").innerHTML = "<div class='alert alert-success'> <a id='link' href='https://github.com/MasterTLM' target='_blank'><b>GitHub: MasterTLM</b> </div>";
 }
 // Lấy đối tượng
