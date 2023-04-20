8566074ec48632d261bc2368f1cc061aad88357f
Master TLM <110680604+MasterTLM@users.noreply.github.com>
2023-04-21 00:28:01 +0700

    update

--- text.js
+++ text.js
@@ -1,16 +1,22 @@
- function myFunction0() {
-            document.getElementById("info").innerHTML = "<div class='alert alert-success'><a id='link' href='https://www.facebook.com/profile.php?id=100009762091194' target='_blank'> <b>Facebook: Minh Master</b> </a> </div>";
-        }
-        function myFunction1() {
-            document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>Số điện thoại: 0767922149</b> </div>";
-        }
-        function myFunction2() {
-            document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>E-Mail: tranleminh106@gmail.com</b> </div>";
-        }
-        function myFunction3() {
-            document.getElementById("info").innerHTML = "<div class='alert alert-success'> <a id='link' href='https://github.com/MasterTLM' target='_blank'><b>GitHub: MasterTLM</b> </div>";
-        }
-        // Lấy đối tượng
-        const animation = document.querySelector(".animation");
-        // Kích hoạt animation
-        animation.classList.add('animate__animated', 'animate__slideInTop');
+function myFunction1() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'><a id='link' href='https://www.facebook.com/profile.php?id=100009762091194' target='_blank'> <b>Facebook: Minh Master</b> </a> </div>";
+}
+function myFunction2() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'><a id='link' href='https://www.youtube.com/channel/UCCVIeKy92nAU8cb3z9dfjOA' target='_blank'> <b>Youtube: Funny Channel</b> </a> </div>";
+}
+function myFunction3() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'><a id='link' href='https://vm.tiktok.com/ZMYvefdQG/' target='_blank'> <b>Tiktok: Minh Master</b> </a> </div>";
+}
+function myFunction4() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>Số điện thoại: 0767922149</b> </div>";
+}
+function myFunction5() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'> <b>E-Mail: tranleminh106@gmail.com</b> </div>";
+}
+function myFunction6() {
+        document.getElementById("info").innerHTML = "<div class='alert alert-success'> <a id='link' href='https://github.com/MasterTLM' target='_blank'><b>GitHub: MasterTLM</b> </div>";
+}
+// Lấy đối tượng
+   const animation = document.querySelector(".animation");
+// Kích hoạt animation
+   animation.classList.add('animate__animated', 'animate__slideInTop');
