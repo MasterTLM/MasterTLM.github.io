545ccbd37319a6868a70e490f619f7c848202fb4
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-05-24 00:20:39 +0700

    Update v2.1.1

--- js/main.js
+++ js/main.js
@@ -1,57 +1,65 @@
+let isShow1 = false;
+let isShow2 = false;
+let isShow3 = false;
+let isShow4 = false;
+const div = document.getElementById("info");
 
-(function ($) {
-    "use strict";
+function toggleDiv(functionName) {
+	if (functionName == "myFunction1") {
+		if (isShow1) {
+			div.style.display = "none";
+			isShow1 = false;
+		} else {
+			div.innerHTML = "<div class='alert alert-primary'><a id='link' href='https://www.facebook.com/profile.php?id=100009762091194' target='_blank'> <b>Facebook: Minh Master</b> </a> </div>";
+			div.style.display = "block";
+			isShow1 = true;
+			isShow2 = false;
+			isShow3 = false;
+			isShow4 = false;
+		}
+	} else if (functionName == "myFunction2") {
+		if (isShow2) {
+			div.style.display = "none";
+			isShow2 = false;
+		} else {
+			div.innerHTML = "<div class='alert alert-danger'><a id='link' href='https://www.youtube.com/channel/UCCVIeKy92nAU8cb3z9dfjOA' target='_blank'> <b>Youtube: Funny Channel</b> </a> </div>";
+			div.style.display = "block";
+			isShow2 = true;
+			isShow1 = false;
+			isShow3 = false;
+			isShow4 = false;
+		}
+	} else if (functionName == "myFunction3") {
+		if (isShow3) {
+			div.style.display = "none";
+			isShow3 = false;
+		} else {
+			div.innerHTML = "<div class='alert alert-secondary'><a id='link' href='https://vm.tiktok.com/ZMYvefdQG/' target='_blank'> <b>Tiktok: Minh Master</b> </a> </div>";
+			div.style.display = "block";
+			isShow3 = true;
+			isShow1 = false;
+			isShow2 = false;
+			isShow4 = false;
+		}
+	} else if (functionName == "myFunction4") {
+		if (isShow4) {
+			div.style.display = "none";
+			isShow4 = false;
+		} else {
+			div.innerHTML = "<div class='alert alert-dark'> <a id='link' href='https://github.com/MasterTLM' target='_blank'><b>GitHub: MasterTLM</b> </div>";
+			div.style.display = "block";
+			isShow4 = true;
+			isShow1 = false;
+			isShow2 = false;
+			isShow3 = false;
+		}
+	}
+}
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
+// // Lấy đối tượng
+//    const animation = document.querySelector(".animation");
+// // Kích hoạt animation
+//    animation.classList.add('animate__animated', 'animate__slideInTop');
+   
+// <a id='link' href='mailto:tranleminh106@gmail.com' target='_blank'>
