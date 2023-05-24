43211dd277a17fcd14c815c5661774499e6e7c2e
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-05-24 18:09:32 +0700

    Pass Test

--- js/quote.js
+++ js/quote.js
@@ -1,9 +1,15 @@
 const button = document.querySelector("#new-quote");
 const p = document.querySelector("#quote");
-const API = "http://api.quotable.io/random";
+const API = "https://api.quotable.io/random";
 
-button.addEventListener("click", () =>{
-    fetch(API).then(res => res.json()).then(data =>{
-        p.innerText = data.content; 
-    }).catch(() => alert("Error fetching quote!"));
+button.addEventListener("click", () => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => {
+        p.innerText = data.content;
+    })
+    .catch(error => {
+        console.log(error);
+        alert("Error fetching quote!");
+    });
 });
