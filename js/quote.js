8efa6ea0c3c1ac89f03e8ccb87928cb17c3e4027
TranLeMinhB2014759 <minhb2014759@student.ctu.edu.vn>
2023-05-25 14:18:15 +0700

    Version 3.0

--- js/quote.js
+++ js/quote.js
@@ -1,11 +1,22 @@
-const button = document.querySelector("#new-quote");
+document.getElementById("toastbtn").onclick = function() {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function(toastEl) {
+      return new bootstrap.Toast(toastEl, { autohide: false })
+    })
+    toastList.forEach(toast => toast.show()) 
+  }  
+
+
+const button = document.querySelector("#toastbtn");
 const p = document.querySelector("#quote");
+const strong = document.querySelector("#author");
 const API = "https://api.quotable.io/random";
 
 button.addEventListener("click", () => {
     fetch(API)
     .then(response => response.json())
     .then(data => {
+        strong.innerText = "--- " + data.author + " ---";
         p.innerText = data.content;
     })
     .catch(error => {
